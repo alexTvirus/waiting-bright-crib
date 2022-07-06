1aa8011a0126075ff77f2318ab505a1575d9d799
Glitch (waiting-bright-crib) <none>
2022-07-06 15:07:05 +0000

    Import of alexTvirus/heroku-node-proxy

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+var url = require('url');
+var querystring = require('querystring');
+var express = require('express');
+var cac = require('./lib/cac.js');
+var Transform = require('stream').Transform;
+
+var app = express();
+
+var cacConfig = {
+    prefix: '/proxy/',
+};
+
+
+
+// this line must appear before any express.static calls (or anything else that sends responses)
+app.use(cac(cacConfig));
+
+// serve up static files *after* the proxy is run
+app.use('/', express.static(__dirname + '/public'));
+
+// this is for users who's form actually submitted due to JS being disabled or whatever
+app.get("/no-js", function(req, res) {
+    // grab the "url" parameter from the querystring
+    var site = querystring.parse(url.parse(req.url).query).url;
+    // and redirect the user to /proxy/url
+    res.redirect(cacConfig.prefix + site);
+});
+
+// for compatibility with gatlin and other servers, export the app rather than passing it directly to http.createServer
+module.exports = app;
