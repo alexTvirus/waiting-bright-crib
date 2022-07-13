fc401cd0d9475aeca2d6dd6d1f3c562c8bf8a996
Glitch (waiting-bright-crib) <none>
2022-07-13 04:24:00 +0000

    👾🚇 Checkpoint ./scripts/google-ana.js:32347864/3174

--- scripts/google-ana.js
+++ scripts/google-ana.js
@@ -2,22 +2,13 @@
 
 var Transform = require('stream').Transform;
 var URL = require('url');
-var google_analytics_id = process.env.GA_ID || null;
+var google_analytics_id = process.env.GA_ID || 'UA-213229401-1';
 module.exports = function () {
 
     function addGa(html) {
         if (google_analytics_id) {
             var ga = [
-                "<script type=\"text/javascript\">",
-                "var _gaq = []; // overwrite the existing one, if any",
-                "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
-                "_gaq.push(['_trackPageview']);",
-                "(function() {",
-                "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
-                "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
-                "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
-                "})();",
-                "</script>"
+                `<script async src='https://www.googletagmanager.com/gtag/js?id=UA-213229401-1'></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', ${google_analytics_id}); </script>`
                 ].join("\n");
             html = html.replace("</body>", ga + "\n\n</body>");
         }
