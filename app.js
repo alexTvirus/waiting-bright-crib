f4c4a6e0d9cc42aef5ada510c5be27a73845cfd3
Glitch (waiting-bright-crib) <none>
2022-07-13 05:36:02 +0000

    🎲🎪 Checkpoint ./app.js:32347864/19

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function validateRequest(data) {
 var cacConfig = {
     prefix: '/proxy/',
       requestMiddleware:[
-        blockAD(),
+        // blockAD(),
         fixUrlOnGlit(),
         fixPornhub(),
         
